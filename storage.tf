b300e779396eb9ecc5d6a44b6b1f901aeb987783
jorge-almeid4 <127250950+jorge-almeid4@users.noreply.github.com>
2023-03-20 13:48:49 -0300

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "default" {
-  name = "first-bucket-crawler-hahaha"
+  name = "first-bucket-crawler-topper"
   storage_class = "REGIONAL"
   location = "us-east1"
 }

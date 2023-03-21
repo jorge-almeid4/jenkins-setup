ea6f270514623899baf14ac1b7c67bdc30886e8e
jorge-almeid4 <127250950+jorge-almeid4@users.noreply.github.com>
2023-03-21 16:03:55 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,6 +8,6 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("/root/.config/gcloud/application_default_credentials.json")
+  credentials = file("SA_key.json")
   project     = "flash-crawler-377213"
 }

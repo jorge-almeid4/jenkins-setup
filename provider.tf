7bd2637691de024a2cd3efe00e360a13bce9b809
jorge-almeid4 <127250950+jorge-almeid4@users.noreply.github.com>
2023-03-21 14:50:39 -0300

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.57.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = "${file("SA_key.json")}"
+  project     = "flash-crawler-377213"
+}
